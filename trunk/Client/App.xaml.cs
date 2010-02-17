cead58e4344185051edac4ad030f37b445b68ca7
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-02-17 23:00:41 +0000

    Started creating basic app structure

--- trunk/Client/App.xaml.cs
+++ trunk/Client/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using DietRecorder.BusinessLeyer;
+using DietRecorder.Model;
 
 namespace DietRecorder.Client
 {
@@ -12,5 +14,10 @@ namespace DietRecorder.Client
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            DetailsListPresenter detailsListPresenter = new DetailsListPresenter(new DetailsList(), new DietLogic());
+            detailsListPresenter.DisplayView();
+        }
     }
 }
