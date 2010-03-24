79596c9630630b6b9e8c72582e7dcfc61fcd1251
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-03-24 22:49:59 +0000

    Started converting to MVVM

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -4,9 +4,12 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
-using DietRecorder.BusinessLeyer;
+using DietRecorder.BusinessLayer;
 using DietRecorder.Model;
 using DietRecorder.DataAccess;
+using DietRecorder.Client.View;
+using DietRecorder.Client.ViewModel;
+using System.Collections.ObjectModel;
 
 namespace DietRecorder.Client
 {
@@ -17,10 +20,18 @@ namespace DietRecorder.Client
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            Repository dietRepository = new Repository();
-            DietLogic dietLogic = new DietLogic(dietRepository);
-            DetailsListPresenter detailsListPresenter = new DetailsListPresenter(new DetailsList(), dietLogic);
-            detailsListPresenter.DisplayView();
+            //Repository dietRepository = new Repository();
+            //DietLogic dietLogic = new DietLogic(dietRepository);
+            //DetailsListPresenter detailsListPresenter = new DetailsListPresenter(new DetailsList(), dietLogic);
+            //detailsListPresenter.DisplayView();
+
+            CustomMeasurementDefinitionViewModel viewModel = new CustomMeasurementDefinitionViewModel();
+            viewModel.MeasurementDefinitions = new ObservableCollection<CustomMeasurementDefinition>();
+            UserView view = new UserView();
+            view.DataContext = viewModel;
+
+            view.Show();
+            
         }
     }
 }
--- Client/UserListPresenter.cs
+++ Client/UserListPresenter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using DietRecorder.BusinessLeyer;
+using DietRecorder.BusinessLayer;
 using DietRecorder.Model;
 using DietRecorder.Client.Common;
 
