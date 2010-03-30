acc67a60a537c9d9ba2990a53c91d6209253c89e
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-03-30 19:21:04 +0000

    Re-added renamed BusinessLayer files/folder

---
+++ BusinessLayer/DietLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using DietRecorder.Model;
+using DietRecorder.DataAccess;
+
+namespace DietRecorder.BusinessLayer
+{
+    public class DietLogic: IDietLogic
+    {
+        private IRepository repository;
+
+        public DietLogic(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public UserList LoadUserList()
+        {
+            UserList userList = repository.Load();
+
+            if (userList != null)
+                return userList;
+            else
+                return new UserList();
+        }
+
+        public void SaveUserList(UserList userList)
+        {
+            repository.Save(userList);
+        }
+
+        public void Delete(object obj)
+        {
+            repository.Delete(obj);
+        }
+    }
+}
