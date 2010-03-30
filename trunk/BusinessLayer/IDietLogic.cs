4b97c38b647a7e47cc1ba393dc3d074b9ad31087
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-03-30 19:21:04 +0000

    Re-added renamed BusinessLayer files/folder

---
+++ trunk/BusinessLayer/IDietLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using DietRecorder.Model;
+
+namespace DietRecorder.BusinessLayer
+{
+    public interface IDietLogic
+    {
+        void SaveUserList(UserList userList);
+        UserList LoadUserList();
+        void Delete(object obj);
+    }
+}
