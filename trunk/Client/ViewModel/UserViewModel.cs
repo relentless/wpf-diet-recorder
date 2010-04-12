24dcae7fc159207fad85d2cb796cdde99ca0a5e4
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-04-12 21:11:46 +0000

    Continued converting to MVVM

--- trunk/Client/ViewModel/UserViewModel.cs
+++ trunk/Client/ViewModel/UserViewModel.cs
@@ -12,12 +12,30 @@ namespace DietRecorder.Client.ViewModel
     {
         private DelegateCommand addUserCommand;
         private DelegateCommand deleteUserCommand;
+        private DelegateCommand newUserCommand;
+        private DelegateCommand cancelNewUserCommand;
+
+        private CustomMeasurementDefinitionViewModel definitionViewModel;
 
         public UserViewModel()
         {
+            SetupCommands();
+            definitionViewModel = new CustomMeasurementDefinitionViewModel();
+            definitionViewModel.MeasurementDefinitions = new ObservableCollection<CustomMeasurementDefinition>();
+            Mode = ViewMode.View;
+        }
+
+        private void SetupCommands()
+        {
             addUserCommand = new DelegateCommand(AddUser);
             deleteUserCommand = new DelegateCommand(DeleteUser);
+            newUserCommand = new DelegateCommand(NewUser);
+            cancelNewUserCommand = new DelegateCommand(CancelNewUser);
         }
+
+        public ObservableCollection<User> Users { get; set; }
+
+        public User SelectedUser { private get; set; }
 
         public ICommand AddUserCommand
         {
@@ -29,14 +47,42 @@ namespace DietRecorder.Client.ViewModel
             get { return deleteUserCommand; }
         }
 
+        public ICommand NewUserCommand
+        {
+            get { return newUserCommand; }
+        }
+
+        public ICommand CancelNewUserCommand
+        {
+            get { return cancelNewUserCommand; }
+        }
+
         private void AddUser()
         {
+            User newUser = new User();
+            newUser.UserName = name;
+            Users.Add(newUser);
+            Name = string.Empty;
 
+            Mode = ViewMode.View;
         }
 
         private void DeleteUser()
         {
+            if (SelectedUser != null)
+            {
+                Users.Remove(SelectedUser);
+            }
+        }
 
+        private void NewUser()
+        {
+            Mode = ViewMode.Edit;
+        }
+
+        private void CancelNewUser()
+        {
+            Mode = ViewMode.View;
         }
 
         private string name;
@@ -56,20 +102,42 @@ namespace DietRecorder.Client.ViewModel
             }
         }
 
-        private ObservableCollection<User> users;
-        public ObservableCollection<User> Users
+        private ViewMode viewMode;
+        public ViewMode Mode
+        {
+            set
+            {
+                if (value != viewMode)
+                {
+                    viewMode = value;
+                    NotifyPropertyChanged("NewUserButtonsVisible");
+                    NotifyPropertyChanged("AddUserButtonsVisible");
+                    NotifyPropertyChanged("NameBoxEditable");
+                }
+            }
+        }
+
+        public bool NewUserButtonsVisible 
         {
             get
             {
-                return users;
+                return viewMode == ViewMode.View;
             }
-            set
+        }
+
+        public bool AddUserButtonsVisible
+        {
+            get
             {
-                if (value != users)
-                {
-                    users= value;
-                    NotifyPropertyChanged("Users");
-                }
+                return viewMode == ViewMode.Edit;
+            }
+        }
+
+        public bool NameBoxEditable
+        {
+            get
+            {
+                return viewMode == ViewMode.Edit;
             }
         }
 
