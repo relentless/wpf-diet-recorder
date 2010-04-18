f5d9aa10598fae429403ea052700ea7a58fc96b0
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-04-18 12:30:02 +0000

    Continued converting to MVVM

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -25,15 +25,19 @@ namespace DietRecorder.Client
             //DetailsListPresenter detailsListPresenter = new DetailsListPresenter(new DetailsList(), dietLogic);
             //detailsListPresenter.DisplayView();
 
-            //CustomMeasurementDefinitionViewModel viewModel = new CustomMeasurementDefinitionViewModel();
-            //viewModel.MeasurementDefinitions = new ObservableCollection<CustomMeasurementDefinition>();
-            UserView view = new UserView();
+            CustomMeasurementDefinitionViewModel definitionViewModel = new CustomMeasurementDefinitionViewModel();
+            definitionViewModel.MeasurementDefinitions = new ObservableCollection<CustomMeasurementDefinition>();
+            
             //view.DataContext = viewModel;
 
-            UserViewModel userViewModel = new UserViewModel();
+            UserViewModel userViewModel = new UserViewModel(definitionViewModel);
             userViewModel.Users = new ObservableCollection<User>();
+
+            UserView view = new UserView();
             view.DataContext = userViewModel;
+
             view.Show();
+            view.DefinitionView.DataContext = userViewModel.DefinitionViewModel;
             
         }
     }
