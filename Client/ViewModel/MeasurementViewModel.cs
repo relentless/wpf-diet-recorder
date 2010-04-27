52e082df5806c1d4c628bd06a5f02352e98a46a4
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-04-27 16:36:34 +0000

    Continued converting to MVVM - Started adding MeasuermentView & ViewModel

---
+++ Client/ViewModel/MeasurementViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using DietRecorder.Model;
+using DietRecorder.Client.Common;
+using System.Windows.Input;
+
+namespace DietRecorder.Client.ViewModel
+{
+    class MeasurementViewModel: INotifyPropertyChanged
+    {
+        private DateTime measurementDate;
+        private double weightKg;
+        private string notes;
+        private User selectedUser;
+        public ObservableCollection<Measurement> Measurements { get; set; }
+
+        #region Commands
+        private DelegateCommand showUsersCommand;
+        private DelegateCommand addMeasurementCommand;
+        private DelegateCommand removeMeasurementCommand;
+
+        public ICommand ShowUsersCommand
+        {
+            get { return showUsersCommand; }
+        }
+
+        public ICommand AddMeasurementCommand
+        {
+            get { return addMeasurementCommand; }
+        }
+
+        public ICommand RemoveMeasurementCommand
+        {
+            get { return removeMeasurementCommand; }
+        }
+        #endregion Commands
+
+        #region Properties with NotifyProperty
+        public DateTime MeasurementDate
+        {
+            get
+            {
+                return measurementDate;
+            }
+            set
+            {
+                if (value != measurementDate)
+                {
+                    measurementDate = value;
+                    NotifyPropertyChanged("MeasurementDate");
+                }
+            }
+        }
+
+        public double WeightKg
+        {
+            get
+            {
+                return weightKg;
+            }
+            set
+            {
+                if (value != weightKg)
+                {
+                    weightKg = value;
+                    NotifyPropertyChanged("WeightKg");
+                }
+            }
+        }
+
+        public String Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                if (value != notes)
+                {
+                    notes = value;
+                    NotifyPropertyChanged("Notes");
+                }
+            }
+        }
+
+        public User SelectedUser
+        {
+            get
+            {
+                return selectedUser;
+            }
+            set
+            {
+                if (value != selectedUser)
+                {
+                    selectedUser = value;
+                    NotifyPropertyChanged("SelectedUser");
+                }
+            }
+        }
+        #endregion Properties with NotifyProperty
+
+        #region Notify Property Stuff
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+        #endregion Notify Property Stuff 
+    }
+}
