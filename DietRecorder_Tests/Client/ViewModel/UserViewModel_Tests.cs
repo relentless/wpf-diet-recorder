79b7eda5aed625062fafff52e517487636fe330a
grantac@hotmail.com <grantac@hotmail.com@96dc72da-1bed-11df-b461-89744e4a0792>
2010-06-01 16:13:21 +0000

    Sorted User view & viewmodel out

--- DietRecorder_Tests/Client/ViewModel/UserViewModel_Tests.cs
+++ DietRecorder_Tests/Client/ViewModel/UserViewModel_Tests.cs
@@ -1,5 +1,8 @@
-﻿using DietRecorder.Client.ViewModel;
+﻿using System.Collections.Generic;
+using DietRecorder.Client.ViewModel;
 using DietRecorder.DataAccess;
+using DietRecorder.Client.Common;
+using DietRecorder.Model;
 using NUnit.Framework;
 using Rhino.Mocks;
 
@@ -12,7 +15,7 @@ namespace DietRecorder_Tests.Client.ViewModel
         public void Name_Set_NotifiesChanged()
         {
             // arrange
-            UserViewModel userVM = new UserViewModel(MockRepository.GenerateStub<IRepository>(), null);
+            UserViewModel userVM = CreateUserViewModel();
 
             PropertyChangedTestHandler handler = new PropertyChangedTestHandler();
             userVM.PropertyChanged += handler.HandlePropertyChanged;
@@ -22,6 +25,90 @@ namespace DietRecorder_Tests.Client.ViewModel
 
             // assert
             Assert.AreEqual("Name", handler.PropertyName);
+        }
+
+        [Test]
+        public void Constructor_MultipleUsers_SelectsFirstUserInList()
+        {
+            // arrange
+            User firstUser = new User("Bill", null, null);
+
+            List<User> users = new List<User>() {
+                firstUser,
+                new User("Ted", null, null)
+            };
+
+            IRepository repository = MockRepository.GenerateStub<IRepository>();
+            repository.Expect(x => x.LoadUserList()).Return(users);
+
+            // act
+            UserViewModel userVM = CreateUserViewModel(repository);
+
+            // assert
+            Assert.AreEqual(firstUser, userVM.SelectedUser);
+        }
+
+        [Test]
+        public void ModifyUserCommand_Called_SetsModeToEdit()
+        {
+            // arrange
+            UserViewModel userVM = CreateUserViewModel();
+            userVM.Mode = ViewMode.View;
+
+            // act
+            userVM.ModifyUserCommand.Execute(null);
+
+            // assert
+            Assert.AreEqual(ViewMode.Edit, userVM.Mode);
+        }
+
+        [Test]
+        public void ModifyUserCommand_Called_EnablesDefinitionViewModel()
+        {
+            // arrange
+            CustomMeasurementDefinitionViewModel definitionVM = MockRepository.GenerateMock<CustomMeasurementDefinitionViewModel>();
+            UserViewModel userVM = CreateUserViewModel(definitionVM);
+            definitionVM.IsEnabled = false;
+
+            // act
+            userVM.ModifyUserCommand.Execute(null);
+
+            // assert
+            Assert.IsTrue(definitionVM.IsEnabled);
+        }
+
+        [Test]
+        public void SelectedUser_Set_SetsNameField()
+        {
+            // arrange
+            User user = new User("Joe", null, null);
+            UserViewModel userVM = CreateUserViewModel();
+
+            // act
+            userVM.SelectedUser = user;
+
+            // assert
+            Assert.AreEqual("Joe", userVM.Name);
+        }
+
+        private static UserViewModel CreateUserViewModel()
+        {
+            return CreateUserViewModel(MockRepository.GenerateStub<IRepository>());
+        }
+
+        private static UserViewModel CreateUserViewModel(CustomMeasurementDefinitionViewModel definitionViewModel)
+        {
+            return CreateUserViewModel(MockRepository.GenerateStub<IRepository>(), definitionViewModel);
+        }
+
+        private static UserViewModel CreateUserViewModel(IRepository repository)
+        {
+            return CreateUserViewModel(repository, MockRepository.GenerateStub<CustomMeasurementDefinitionViewModel>());
+        }
+
+        private static UserViewModel CreateUserViewModel(IRepository repository, CustomMeasurementDefinitionViewModel definitionViewModel)
+        {
+            return new UserViewModel(repository, definitionViewModel);
         }
     }
 }
